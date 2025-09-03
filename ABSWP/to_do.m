100367ae6619a48abae9e6af4df5cf39c1974169
dtorres <benjat3@gmail.com>
2025-09-03 03:37:41 +0000

    Actualizado to_do

--- ABSWP/to_do.m
+++ ABSWP/to_do.m
@@ -4,17 +4,7 @@ He leído hasta **Cap. 6 (Listas)**, pero me faltan algunos temas.
 Acá está la guía con páginas y secciones para repasar:
 
 ---
-
-## ✅ Tipos de excepciones
-- **Capítulo 3**  
-  - *The try and except Statements* → **p. 97–99**  
-    (ValueError, ZeroDivisionError, Exception, etc.)
-
+Llegué hasta la página 98 del cap 5, luego tengo que continuar con el cap 6 de listas.
+Tengo que ver Assertions
 ---
 
-## ✅ Mensajes de error claros
-- **Capítulo 3**  
-  - *The try and except Statements* → **p. 99**  
-    (Uso de `except ValueError:`, `except Exception as err:` y `print(err)`)
-
----
