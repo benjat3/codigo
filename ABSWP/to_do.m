c7740c869ab8598ec0ae95ed5b7ed708eefd1653
dtorres <benjat3@gmail.com>
2025-09-06 00:42:52 +0000

    Algunos ejemplos con listas

--- ABSWP/to_do.m
+++ ABSWP/to_do.m
@@ -4,12 +4,9 @@ He leído hasta **Cap. 6 (Listas)**, pero me faltan algunos temas.
 Acá está la guía con páginas y secciones para repasar:
 
 ---
-    Tengo que hacer los ejemplos marcados en el Kindle del cap 6 y luego seguir
-
 
 # Al finalizar el libro (o cuando empiece la parte de proyectos, para practicar)
     Tomar todas las preguntas, intentar responderlas y meterlas en el anki.
-
 
 ---
 
