6bce85a2e73e4e49b044b1410d47d82943bb505c
dtorres <benjat3@gmail.com>
2025-09-07 03:31:52 +0000

    Hice un ej del cap 7

--- ABSWP/to_do.m
+++ ABSWP/to_do.m
@@ -5,7 +5,7 @@ Acá está la guía con páginas y secciones para repasar:
 
 ---
 
-1. Hacer los programas de práctica del capítulo 6 (pag. 136)
+1. Continuar con "Checking Whether a Key Exists" del cap 7.
 
 # Al finalizar el libro (o cuando empiece la parte de proyectos, para practicar)
     Tomar todas las preguntas, intentar responderlas y meterlas en el anki.
